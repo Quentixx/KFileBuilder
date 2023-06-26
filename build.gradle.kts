e615bdbf9b34f4f5a63ba5bc92f26da30d7f638e
Quentin <nelsonsims@hotmail.fr>
2023-06-26 19:04:49 +0200

    chore: Add deps for serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
     id("org.jetbrains.compose") version "1.4.0"
 }
 
@@ -17,32 +18,19 @@ repositories {
 
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
-
 }
 
 dependencies {
-
     implementation("ch.qos.logback:logback-classic:1.4.8")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
 
-
-
     implementation(compose.desktop.currentOs)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.5.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 
     testImplementation(kotlin("test"))
 }
-
-//tasks.test {
-//    useJUnitPlatform()
-//}
-//
-//kotlin {
-//    jvmToolchain(11)
-//}
-//
-//application {
-//    mainClass.set("MainKt")
-//}
 
 compose.desktop {
     application {
