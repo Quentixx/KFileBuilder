d58fb50c20e73cbe2a5c7da990fee137f24d6866
Quentin <nelsonsims@hotmail.fr>
2023-06-27 17:56:26 +0200

    chore: Creating a screen manager to handle multiple screens in one tab

---
+++ src/main/kotlin/tab/template/TemplateScreenManager.kt
@@ -0,0 +1,29 @@
+package tab.template
+
+import androidx.compose.runtime.mutableStateOf
+
+/**
+ * Enum class representing different screens in Templates tab.
+ */
+enum class TemplateScreen {
+    MAIN_VIEW,
+    CREATE_TEMPLATE_VIEW,
+    EDIT_TEMPLATE_VIEW
+}
+
+/**
+ * Class responsible for managing the current template screen.
+ */
+class TemplateScreenManager {
+    // The current template screen state.
+    val currentView = mutableStateOf(TemplateScreen.MAIN_VIEW)
+
+    /**
+     * Navigates to the specified template screen.
+     *
+     * @param screen The target template screen to navigate to.
+     */
+    fun navigateTo(screen: TemplateScreen) {
+        currentView.value = screen
+    }
+}
