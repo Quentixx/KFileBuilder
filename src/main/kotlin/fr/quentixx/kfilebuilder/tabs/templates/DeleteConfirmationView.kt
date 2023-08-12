8977aca62fd43c20b9f95831742c722b1e2b80e8
Quentin <nelsonsims@hotmail.fr>
2023-08-12 17:22:55 +0200

    chore(deleteTemplate): Use generic window to confirm template deletion

--- src/main/kotlin/fr/quentixx/kfilebuilder/tabs/templates/DeleteConfirmationView.kt
+++ src/main/kotlin/fr/quentixx/kfilebuilder/tabs/templates/DeleteConfirmationView.kt
@@ -1,91 +1,22 @@
 package fr.quentixx.kfilebuilder.tabs.templates
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.rememberWindowState
-import fr.quentixx.kfilebuilder.color.customGreen
-import fr.quentixx.kfilebuilder.ext.setOnHoverHandCursorEnabled
+import fr.quentixx.kfilebuilder.components.GenericConfirmWindow
 import fr.quentixx.kfilebuilder.json.TemplateStorageService
 import fr.quentixx.kfilebuilder.tabs.templates.commons.TemplateScreen
 import fr.quentixx.kfilebuilder.tabs.templates.commons.TemplateScreenManager
 
+/**
+ * Represents the confirmation view of template deletion.
+ * @param screenManager To navigate through the different view.
+ */
 @Composable
 fun DeleteTemplateView(screenManager: TemplateScreenManager) {
-    Window(
-        title = "Confirmation",
-        onCloseRequest = {
-            screenManager.navigateTo(TemplateScreen.MAIN_VIEW)
-        },
-        resizable = false,
-        focusable = false,
-        alwaysOnTop = true,
-        state = rememberWindowState(
-            position = WindowPosition(Alignment.Center),
-            size = DpSize(500.dp, 300.dp)
-        )
-    ) {
-        Column(
-            Modifier
-                .fillMaxSize()
-                .background(Color.Gray),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Êtes-vous sûr de vouloir supprimer ce modèle ?")
-            Spacer(Modifier.height(64.dp))
-            Row(
-                Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                ConfirmDeleteTemplateButton(screenManager)
-
-                Spacer(Modifier.width(32.dp))
-
-                UnconfirmDeleteTemplateButton(screenManager)
-            }
-        }
-    }
-}
-
-@Composable
-private fun ConfirmDeleteTemplateButton(
-    screenManager: TemplateScreenManager
-) {
-    Button(
-        onClick = {
+    GenericConfirmWindow(
+        confirmText = "Voulez-vous vraiment supprimer ce modèle ?",
+        onBack = { screenManager.navigateTo(TemplateScreen.MAIN_VIEW) },
+        onConfirm = {
             TemplateStorageService.delete(screenManager.selectedTemplate!!.uuid)
-            screenManager.navigateTo(TemplateScreen.MAIN_VIEW)
-        },
-        modifier = Modifier.setOnHoverHandCursorEnabled(),
-        colors = ButtonDefaults.buttonColors(customGreen)
-    ) {
-        Text("Oui")
-    }
-}
-
-@Composable
-private fun UnconfirmDeleteTemplateButton(
-    screenManager: TemplateScreenManager
-) {
-    Button(
-        onClick = {
-            screenManager.navigateTo(TemplateScreen.MAIN_VIEW)
-        },
-        modifier = Modifier.setOnHoverHandCursorEnabled(),
-        colors = ButtonDefaults.buttonColors(Color.Red)
-    ) {
-        Text("Non")
-    }
+        }
+    )
 }
