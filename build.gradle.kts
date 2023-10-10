4bc563ef8660aa3edfd7ad913c0e0897c401ef3f
Quentin <nelsonsims@hotmail.fr>
2023-10-10 13:34:26 +0200

    deps(shadow): Add shadow 7.1.1 to compile the project with deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm") version "1.8.20"
     kotlin("plugin.serialization") version "1.8.20"
     id("org.jetbrains.compose") version "1.4.0"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 group = "fr.quentixx"
@@ -28,7 +30,6 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.compose.ui:ui-test-junit4-desktop:1.4.1")
-
 }
 
 tasks {
@@ -39,12 +40,30 @@ tasks {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "fr.quentixx.kfilebuilder.Main"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "KotlinJvmComposeDesktopApplication"
             packageVersion = "1.0.0"
         }
+    }
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    named<ShadowJar>("shadowJar") {
+        manifest {
+            attributes(
+                "Main-Class" to "fr.quentixx.kfilebuilder.MainKt"
+            )
+        }
+
+        from(sourceSets.main.get().allSource)
     }
 }
